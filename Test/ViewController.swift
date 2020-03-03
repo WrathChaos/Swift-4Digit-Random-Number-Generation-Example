79e3980e7f3ff08e46e3326e6d4bad4ad9c12419
FreakyCoder <kurayogun@gmail.com>
2020-03-03 10:50:36 +0300

    Initial Commit with Working Example 🥳

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var fourDigitNumber: String {
+         var result = ""
+         repeat {
+             // Create a string with a random number 0...9999
+             result = String(format:"%04d", arc4random_uniform(10000) )
+         } while result.count < 4
+         return result
+        }
+        
+        // USAGE
+        let password = fourDigitNumber
+        print("Generated Password : \(password)")
     }
-
-
 }
 
